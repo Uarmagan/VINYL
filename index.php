d0896de81562a2da6f44e0bab3f8252d818c4f5c
Ugur Armagan <ugurarmagan93@gmail.com>
2018-04-11 21:07:08 -0500

    removed all comments

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
 //this is a test commit
-<<<<<<< HEAD
-=======
-//issa is the person who wrote this
-
-//test commit by Brandon
-//again again again
-//lskdjf;alksjf;lksadjf;lkasdjf;lkdsjf;lkjd;lfkjl;askdf
->>>>>>> 45dbea44dfc20aa6f9886a22a0f3b33e358ae3c4
