77aed7130dcb3322198f377f2b841adef9da29d5
Ugur Armagan <ugurarmagan93@gmail.com>
2018-04-13 14:18:26 -0500

    Added Bootstrap CDN, NavBar, and small styling changes in catalog

--- catalog.php
+++ catalog.php
@@ -5,7 +5,7 @@ require('connect.php');
 ?>
 
 <div class="search">
-    <input type="searchBar" size="100px" style="border:1px solid #333; margin-left:20px;">
+    <input type="searchBar" size="100px" style="border:1px solid #333; margin:20px;">
     <button>Search</button>
 </div>
 
@@ -17,12 +17,12 @@ require('connect.php');
             echo '<div style="display:flex; flex-wrap:wrap;">';
 
             while($row = mysqli_fetch_array($result)){
-                echo '<div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px; margin:20px; width:200px;">';
+                echo '<div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px; margin:20px; width:250px;">';
                     echo '<form method="post" action="">';
-                        echo '<h4>Album Name: '.$row['albumName'].'</h4>';
-                        echo '<h4> Artist:'. $row['artistName'] .'</h4>';
-                        echo '<h4> Price: $'. $row['cost'] .'</h4>';
-                        echo '<h4> Qty Available: $'. $row['quantity'] .'</h4>';
+                        echo '<h5>Album Name: '.$row['albumName'].'</h5>';
+                        echo '<h5> Artist: '. $row['artistName'] .'</h5>';
+                        echo '<h5> Price: $'. $row['cost'] .'</h5>';
+                        echo '<h5> Qty Available: '. $row['quantity'] .'</h5>';
                         echo '<input type="text" name="quantity" class="form-control" value="1" />';
                         echo '<input type="submit" name="add_to_cart" style="margin-top:5px;" value="Add to Cart" />';
                         echo '<hr>';
@@ -38,6 +38,10 @@ require('connect.php');
     ?>
 </div>
 
-
+<!-- browser sync script -->
+<script id="__bs_script__">//<![CDATA[
+        document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.23.5'><\/script>".replace("HOST", location.hostname));
+    //]]></script>
+    
 <?php
 include('includes/footer.html');
