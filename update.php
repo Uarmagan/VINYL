a31f622e8aa76f208bee5faa02247b9d709ae6d9
issaissa12 <isissa94@gmail.com>
2018-04-17 15:54:46 -0500

    owner crud

---
+++ update.php
@@ -0,0 +1,20 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <?php
+
+
+
+
+    ?>
+</head>
+<body>
+    <form method="POST">
+        <input name="" placeholder="">
+        <input name="" placeholder="">
+        <input name="" placeholder="">
+        <input name="" placeholder="">
+        <input name="" placeholder="">
+    </form>
+</body>
+</html>
