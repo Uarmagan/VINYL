f50899b854671285b345b01301f02bfce775a8f1
issaissa12 <isissa94@gmail.com>
2018-04-19 13:42:33 -0500

    update work

--- update.php
+++ update.php
@@ -10,10 +10,10 @@
 </head>
 <body>
     <form method="POST">
-        <input name="" placeholder="">
-        <input name="" placeholder="">
-        <input name="" placeholder="">
-        <input name="" placeholder="">
+        <input name="albumName" placeholder="Album Name">
+        <input name="artistName" placeholder="Artist Name">
+        <input name="cost" placeholder="Cost">
+        <input name="yearRelease" placeholder="">
         <input name="" placeholder="">
     </form>
 </body>
