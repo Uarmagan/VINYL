c440a4aaba7e6b88d40480201096566985de7cf2
Ugur Armagan <ugurarmagan93@gmail.com>
2018-04-25 13:19:17 -0500

    remove button implemented in shopping cart and navbar is personalized

--- shoppingCart.php
+++ shoppingCart.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
 include('includes/header.html');
+
+if(isset($_GET["action"]))
+ {
+      if($_GET["action"] == "delete")
+      {
+           foreach($_SESSION["cart"] as $keys => $values)
+           {
+                if($values["itemID"] == $_GET["id"])
+                {
+                     unset($_SESSION["cart"][$keys]);
+                     echo '<script>alert("Item Removed")</script>';
+                     echo '<script>window.location="shoppingCart.php"</script>';
+                }
+           }
+      }
+ }
+ 
 ?>
 <h3>Order Details</h3>
 <table style="width:600px;">
@@ -24,6 +41,7 @@ include('includes/header.html');
         <td><?php echo $values["quantity"]; ?></td>
         <td>$ <?php echo $values["cost"]; ?></td>
         <td>$ <?php echo number_format((float) $values["quantity"] * (float) $values["cost"], 2); ?></td>
+        <td><a href="shoppingCart.php?action=delete&id=<?php echo $values["itemID"]; ?>" style="color:red;">Remove</a></td>
     </tr>
     <?php
             $total = intval($total + ((float)$values["quantity"] * (float)$values["cost"]));
