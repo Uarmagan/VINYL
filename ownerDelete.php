afb12b6fff62dbe6e491a7594d9ecdb7919fac58
issaissa12 <isissa94@gmail.com>
2018-04-27 00:29:16 -0500

    d

--- ownerDelete.php
+++ ownerDelete.php
@@ -1,6 +1,6 @@
  <?php
     session_start();
-    //include('includes/header.html');
+    include('includes/header.html');
     $page_title = 'Delete Inventory';
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
